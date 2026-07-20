b1d571769c49f8ff7948446f5361bd33f00634d6
Daniel Sarda <daniel.sarda@processcontrol.es>
2026-07-20 08:54:25 +0200

    [REM] pc_mes_kiosk(_mrp): demo migrada a Shop Floor nativo + QC estandar

--- pc_mes_kiosk/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2026 Process Control (https://www.processcontrol.es)
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from . import models
-from . import controllers
--- pc_mes_kiosk_mrp/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright 2026 Process Control (https://www.processcontrol.es)
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from . import test_kiosk_mrp
